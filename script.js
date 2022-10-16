033dd40ce64b2a43ef2cf048b4c6e1e6d28698b9
LauraSouzaCastro <laurasouzacastro1@gmail.com>
2022-10-16 02:43:51 -0300

    Versao final sem os bonus

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let prato, bebida, sobremesa, valorPrato, valorBebida, valorSobremesa;
+let prato, bebida, sobremesa, valorPrato, valorBebida, valorSobremesa, verifica;
 
 function selecionarPrato(item){
     if(prato != null){
@@ -8,7 +8,7 @@ function selecionarPrato(item){
     prato = item.children[1].innerHTML;
     valorPrato = item.children[3].innerHTML;
     item.classList.add("selecionado");
-    verifica();
+    verificar();
 }
 function selecionarBebida(item){
     if(bebida != null){
@@ -18,7 +18,7 @@ function selecionarBebida(item){
     bebida = item.children[1].innerHTML;
     valorBebida = item.children[3].innerHTML;
     item.classList.add("selecionado");
-    verifica();
+    verificar();
 }
 function selecionarSobremesa(item){
     if(sobremesa != null){
@@ -28,13 +28,57 @@ function selecionarSobremesa(item){
     sobremesa = item.children[1].innerHTML;
     valorSobremesa = item.children[3].innerHTML;
     item.classList.add("selecionado");
-    verifica();
+    verificar();
 }
-function verifica(){
+function verificar(){
     if(prato != null && bebida != null && sobremesa != null){
         const botao = document.querySelector(".botao");
         botao.classList.add("botaoVerde");
         botao.innerHTML = "Fechar pedido";
+        verifica = 1;
+    }
+}
+function finalizarPedido(){
+    if(verifica != null){
+        let valorPratoConvertido = ""; 
+        let valorBebidaConvertido = "";
+        let valorSobremesaConvertido = "";
+        for(let i = 3; i < valorPrato.length; i++){
+            if(valorPrato[i] == ","){
+                valorPratoConvertido += ".";
+            }else{
+                valorPratoConvertido += valorPrato[i];
+            }
+        }
+        for(let i = 3; i < valorBebida.length; i++){
+            if(valorBebida[i] == ","){
+                valorBebidaConvertido += ".";
+            }else{
+                valorBebidaConvertido += valorBebida[i];
+            }
+        }
+        for(let i = 3; i < valorSobremesa.length; i++){
+            if(valorSobremesa[i] == ","){
+                valorSobremesaConvertido += ".";
+            }else{
+                valorSobremesaConvertido += valorSobremesa[i];
+            }
+        }
+        let valorPratoConvertidoNumber = Number(valorPratoConvertido);
+        let valorBebidaConvertidoNumber = Number(valorBebidaConvertido);
+        let valorSobremesaConvertidoNumber = Number(valorSobremesaConvertido);
+        let valorTotal =  (valorPratoConvertidoNumber + valorBebidaConvertidoNumber + valorSobremesaConvertidoNumber).toFixed(2);
+        valorTotal += "";
+        let precoTotal = "";
+        for(let i = 0; i < valorTotal.length; i++){
+            if(valorTotal[i] == "."){
+                precoTotal += ",";
+            }else{
+                precoTotal += valorTotal[i];
+            }
+        }
+        let minhaString = `Olá, gostaria de fazer o pedido:\n- Prato: ${prato}\n- Bebida: ${bebida}\n- Sobremesa: ${sobremesa}\nTotal: R$ ${precoTotal}`;
+        window.location.href = `https://wa.me/5562996330374?text=${encodeURIComponent(minhaString)}`;
     }
 }
 
