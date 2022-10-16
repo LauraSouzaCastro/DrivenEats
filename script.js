0dbdebd4dd5d0c3db3c99744c76eba8fb763cb08
LauraSouzaCastro <laurasouzacastro1@gmail.com>
2022-10-16 00:26:28 -0300

    Selecao de itens

---
+++ script.js
@@ -0,0 +1,39 @@
+let prato, bebida, sobremesa, valorPrato, valorBebida, valorSobremesa;
+
+function selecionarPrato(item){
+    if(prato != null){
+        const antigo = document.querySelector(".prato .selecionado");
+        antigo.classList.remove("selecionado")
+    }
+    prato = item.children[1].innerHTML;
+    valorPrato = item.children[3].innerHTML;
+    item.classList.add("selecionado");
+    verifica();
+}
+function selecionarBebida(item){
+    if(bebida != null){
+        const antigo = document.querySelector(".bebida .selecionado");
+        antigo.classList.remove("selecionado")
+    }
+    bebida = item.children[1].innerHTML;
+    valorBebida = item.children[3].innerHTML;
+    item.classList.add("selecionado");
+    verifica();
+}
+function selecionarSobremesa(item){
+    if(sobremesa != null){
+        const antigo = document.querySelector(".sobremesa .selecionado");
+        antigo.classList.remove("selecionado")
+    }
+    sobremesa = item.children[1].innerHTML;
+    valorSobremesa = item.children[3].innerHTML;
+    item.classList.add("selecionado");
+    verifica();
+}
+function verifica(){
+    if(prato != null && bebida != null && sobremesa != null){
+        const botao = document.querySelector(".botao");
+        botao.classList.add("botaoVerde");
+        botao.innerHTML = "Fechar pedido";
+    }
+}
