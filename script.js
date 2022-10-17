7badc88da253d810faa83f4fed8fcf04ac158cf1
LauraSouzaCastro <laurasouzacastro1@gmail.com>
2022-10-16 23:57:23 -0300

    versao final com os bonus

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let prato, bebida, sobremesa, valorPrato, valorBebida, valorSobremesa, verifica;
+let prato, bebida, sobremesa, valorPrato, valorBebida, valorSobremesa, verifica, precoTotal, valorTotal, valorBebidaConvertidoNumber, valorPratoConvertidoNumber, valorSobremesaConvertidoNumber;
 
 function selecionarPrato(item){
     if(prato != null){
@@ -49,24 +49,50 @@ function converte(valorString){
     }
     return Number(valorConvertido);
 }
+function converteVirgula(valor){
+    valor = valor.toFixed(2);
+    valor += "";
+    let preco = "";
+    for(let i = 0; i < valor.length; i++){
+        if(valor[i] == "."){
+            preco += ",";
+        }else{
+            preco += valor[i];
+        }
+    }
+    return preco;
+}
+function confirmarPedido(){
+    if(verifica != null){
+        const divConfima = document.querySelector(".some");
+        divConfima.classList.remove("some");
+        const infoPrato = document.querySelector(".infoPrato");
+        const infoBebida = document.querySelector(".infoBebida");
+        const infoSobremesa = document.querySelector(".infoSobremesa");
+        const infoTotal = document.querySelector(".infoTotal");
+        valorPratoConvertidoNumber = converte(valorPrato);
+        valorBebidaConvertidoNumber = converte(valorBebida);
+        valorSobremesaConvertidoNumber = converte(valorSobremesa);
+        valorTotal =  (valorPratoConvertidoNumber + valorBebidaConvertidoNumber + valorSobremesaConvertidoNumber);
+        infoPrato.children[0].innerHTML = prato;
+        infoPrato.children[1].innerHTML = converteVirgula(valorPratoConvertidoNumber);
+        infoBebida.children[0].innerHTML = bebida;
+        infoBebida.children[1].innerHTML = converteVirgula(valorBebidaConvertidoNumber);
+        infoSobremesa.children[0].innerHTML = sobremesa;
+        infoSobremesa.children[1].innerHTML = converteVirgula(valorSobremesaConvertidoNumber);
+        infoTotal.children[1].innerHTML = `R$ ${converteVirgula(valorTotal)}`;
+    }  
+}
 function finalizarPedido(){
     if(verifica != null){
-        let valorPratoConvertidoNumber = converte(valorPrato);
-        let valorBebidaConvertidoNumber = converte(valorBebida);
-        let valorSobremesaConvertidoNumber = converte(valorSobremesa);
-        let valorTotal =  (valorPratoConvertidoNumber + valorBebidaConvertidoNumber + valorSobremesaConvertidoNumber).toFixed(2);
-        valorTotal += "";
-        let precoTotal = "";
-        for(let i = 0; i < valorTotal.length; i++){
-            if(valorTotal[i] == "."){
-                precoTotal += ",";
-            }else{
-                precoTotal += valorTotal[i];
-            }
-        }
+        precoTotal = converteVirgula(valorTotal);
         let nome = prompt("Qual é o seu nome?");
         let endereco = prompt("Qual é o seu endereco?");
         let minhaString = `Olá, gostaria de fazer o pedido:\n- Prato: ${prato}\n- Bebida: ${bebida}\n- Sobremesa: ${sobremesa}\nTotal: R$ ${precoTotal}\n\nNome: ${nome}\nEndereço: ${endereco}`;
-        window.location.href = `https://wa.me/5562996330374?text=${encodeURIComponent(minhaString)}`;
+        window.location.href = `https://wa.me/5562994174964?text=${encodeURIComponent(minhaString)}`;
     }
+}
+function cancelar(){
+    const divConfima = document.querySelector(".fundo");
+    divConfima.classList.add("some");
 }
